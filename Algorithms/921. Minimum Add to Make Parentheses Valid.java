0474ccb2a2b23f5437960cea8e131734c996ee3f
zyb4950045 <1014945325@qq.com>
2018-10-26 02:02:55 +0800

    提交Leetcode 921. Minimum Add to Make Parentheses Valid

--- Algorithms/921. Minimum Add to Make Parentheses Valid.java
+++ Algorithms/921. Minimum Add to Make Parentheses Valid.java
@@ -1,13 +1,10 @@
-//CHECKSTYLE:OFF
-package Leetcode;
-
 /**
- * Created by zero on 2017/9/18.
+ * 这水题居然是medium难度的。。。
  */
 class Solution {
     public int minAddToMakeValid(String S) {
         if (S == null || S.length() == 0) return 0;
-        int ans = 0, left = 0, right = 0;
+        int ans = 0, left = 0;
         for(char ch : S.toCharArray()) {
             if (ch == ')') {
                 if (left == 0) {
@@ -20,15 +17,5 @@ class Solution {
             }
         }
         return ans + left;
-    }
-}
-
-public class Main {
-    public static void main(String[] args){
-//        FileInputStream file = new FileInputStream('in.txt');
-//        System.setIn(file);
-        Solution solution = new Solution();
-        String str = "((()))";
-        System.out.println(solution.minAddToMakeValid(str));
     }
 }
