c14f0362e48920b5a923b8db94d2559943cf8cbd
zyb4950045 <1014945325@qq.com>
2018-10-27 00:08:33 +0800

    提交Leetcode 890. Find and Replace Pattern

--- Algorithms/890. Find and Replace Pattern.java
+++ Algorithms/890. Find and Replace Pattern.java
@@ -1,13 +1,3 @@
-//CHECKSTYLE:OFF
-package Leetcode;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-/**
- * Created by zero on 2017/9/18.
- */
 class Solution {
     private int[] vis1 = new int[27];
     private int[] vis2 = new int[27];
@@ -17,11 +7,12 @@ class Solution {
         for (int i = 0; i < word.length(); i++) {
             int pos1 = word.charAt(i) - 96;
             int pos2 = pattern.charAt(i) - 96;
-            System.out.println(vis1[pos1] + "," + vis2[pos2]);
+            //System.out.println("i:" + i + ", " + "pos1:" + pos1 + ", " + "pos2:" + pos2);
             if (vis1[pos1] == 0 && vis2[pos2] == 0) {
                 vis1[pos1] = pos2;
                 vis2[pos2] = pos1;
-            } else if (vis1[pos1] > 0 && vis2[pos2] > 0 && vis1[pos1] == pos2) {
+            } else if (vis1[pos1] > 0 && vis2[pos2] > 0 &&
+                    vis1[pos1] == pos2) {
                 continue;
             } else {
                 return false;
@@ -40,17 +31,5 @@ class Solution {
             Arrays.fill(vis2, 0);
         }
         return res;
-    }
-}
-
-public class Main {
-    public static void main(String[] args){
-//        FileInputStream file = new FileInputStream('in.txt');
-//        System.setIn(file);
-        Solution solution = new Solution();
-        String[] words = {"abc","deq","mee","aqq","dkd","ccc"};
-        String pattern = "bba";
-        List<String> res = solution.findAndReplacePattern(words, pattern);
-        System.out.println(Arrays.toString(res.toArray()));
     }
 }
